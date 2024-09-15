ad5a0992f1cf993dd79893c82b30a05d8bf186f2
Lucas Miguez <lucasmiguez46@gmail.com>
2024-09-15 15:31:03 -0300

    component agregados, left, top, right y main-wrapper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { LeftBarComponent } from './components/left-bar/left-bar.component';
+import { RightBarComponent } from './components/right-bar/right-bar.component';
+import { MainWrapperComponent } from './components/main-wrapper/main-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    LeftBarComponent,
+    RightBarComponent,
+    MainWrapperComponent
   ],
   imports: [
     BrowserModule,
